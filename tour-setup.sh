396ddb720d2c9caf6c1d4e8df80dc098f6384edf
Twystidceed <kjwacikowski@gmail.com>
2020-12-11 04:32:39 -0500

    Update tour-setup.sh

--- tour-setup.sh
+++ tour-setup.sh
@@ -1,34 +1,78 @@
 #!/bin/bash
-# TourCoin TOUR Masternode Setup Script V1.0 for Ubuntu 16.04 LTS
+# Twystid's Masternode Setup Script V3.2 for Ubuntu
 #
 # Script will attempt to auto detect primary public IP address
-# and generate masternode private key unless specified in command line
-#
+# This script is capable of installing with or without swap depending on your VPS
 # Usage:
-# bash tour-setup.sh 
+# bash bare-setup.sh 
 #
 
 #Color codes
 RED='\033[0;91m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
 NC='\033[0m' # No Color
 
-#MON TCP port
+#TCP port
 PORT=5457
 RPC=5458
 
-#OTHER VARIABLES
-NAME=TourCoin
+#GLOBAL VARIABLES - Check the daemon deployment section for proper deployment
+
+#this is the Github Source for the binaries
+
+#Version for ubuntu 18.04
+SOURCEA=https://github.com/Touriva/TOUR/releases/download/v1.1.0/Tour_linux1.1.0-1804.tar.xz
+
+#Version for ubuntu 16.04
+SOURCEB=https://github.com/Touriva/TOUR/releases/download/v1.1.0/Tour_linux1.1.0-1604.tar.gz
+
+#The archive itself from the source
+#18.04
+ARCHIVEA=Tour_linux1.1.0-1804.tar.xz
+#16.04
+ARCHIVEB=Tour_linux1.1.0-1604.tar.gz
+
+#ADDNODES
+ADDNODEA=144.91.87.7:5457
+ADDNODEB=92.53.120.38:5457
+ADDNODEC=185.178.47.191:5457
+
+#name of the folder created with the git clone when clonign the repository
+FOLDER=TOUR-MN-setup
+
+#official name
+NAME='Touriva'
+
+#name2 is the actual name of the binary when installed on VPS [CASE SENSISTIVE]
+NAME2=tour
+
+#Simply the Ticker of the coin for referenceing in the script - no usage case not sensitive
 TICKER=TOUR
-HIDDEN=tour
+
+#actual name of the hidden folder for the coin [CASE SENSITIVE]
+HIDDEN=.tour
+
+#Actual name of the conf file in the hidden folder [CASE SENSITIVE]
 CONF=tour.conf
+
+#actual name od the coin daemon [CASE SENSITIVE]
 DAEMON=tourd
+
+#Actual name of the coin daemon -cli [CASE SENSITIVE]
 CLI=tour-cli
+
+#name of the monitor script [CASE SENSITIVE]
 MONITOR=tourmon.sh
 
-#LINES TO MANUALLY CHANGE
-# 44, 
+#only enable if needed due to binaries being extracted to a second folder within the cloned folder
+#FOLDER2=if needed 
+
+
+#####################################
+#     END OF GLOBAL VARIABLES       #
+#####################################
 
 #Clear keyboard input buffer
 function clear_stdin { while read -r -t 0; do read -r; done; }
@@ -38,16 +82,16 @@ function delay { echo -e "${GREEN}Sleep for $1 seconds...${NC}"; sleep "$1"; }
 
 #Stop daemon if it's already running
 function stop_daemon {
-    if pgrep -x '$DAEMON' > /dev/null; then
+    if pgrep -x $DAEMON > /dev/null; then
         echo -e "${YELLOW}Attempting to stop $DAEMON${NC}"
         $CLI stop
         delay 30
-        if pgrep -x 'tourd' > /dev/null; then
+        if pgrep -x $DAEMON > /dev/null; then
             echo -e "${RED}$DAEMON daemon is still running!${NC} \a"
             echo -e "${RED}Attempting to kill...${NC}"
-            pkill $DAEMON
+            pkill -9 $DAEMON
             delay 30
-            if pgrep -x '$DAEMON' > /dev/null; then
+            if pgrep -x $DAEMON > /dev/null; then
                 echo -e "${RED}Can't stop $DAEMON! Reboot and try again...${NC} \a"
                 exit 2
             fi
@@ -55,15 +99,11 @@ function stop_daemon {
     fi
 }
 
-#Function detect_ubuntu
 
- if [[ $(lsb_release -d) == *16.04* ]]; then
-   UBUNTU_VERSION=16
-else
-   echo -e "${RED}You are not running Ubuntu 16.04, Installation is cancelled.${NC}"
-   exit 1
 
-fi
+
+#Process command line parameters
+genkey=$1
 
 clear
 echo -e "${PURPLE}####### #     # #     #  #####  ####### ### ######  ${NC}"
@@ -88,13 +128,8 @@ echo -e "${GREEN}This script contains multiple options - please choose proper se
 echo -e
 echo -e 
 sleep 5
-#Process command line parameters
-genkey=$1
 
-clear
-
-echo -e "${YELLOW}$NAME Masternode Setup Script V1 for Ubuntu 16.04 LTS${NC}"
-echo "Do you want me to generate a masternode private key for you? [y/n]"
+echo -e "${YELLOW}Do you want me to generate a masternode private key for you? [y/n]${NC}"
   read DOSETUP
 if [[ $DOSETUP =~ "n" ]] ; then
           read -e -p "Enter your private key:" genkey;
@@ -143,7 +178,8 @@ sudo apt-get -y install wget nano htop jq
 sudo apt-get -y install libzmq3-dev
 sudo apt-get -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
 sudo apt-get -y install libevent-dev
-sudo apt-get install unzip
+sudo apt-get install -y unzip
+sudo apt-get install -y dtrx
 sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get -y update
@@ -154,7 +190,7 @@ sudo service fail2ban restart
 sudo apt-get install -y libdb5.3++-dev libdb++-dev libdb5.3-dev libdb-dev && ldconfig
 sudo apt-get install -y libzmq3-dev build-essential libssl-dev libboost-all-dev libqrencode-dev libminiupnpc-dev libboost-system1.58.0 libboost1.58-all-dev libdb4.8++ libdb4.8 libdb4.8-dev libdb4.8++-dev libevent-pthreads-2.0-5
    fi
-
+sudo apt-get install libboost-all-dev
 #Network Settings
 echo -e "${GREEN}Installing Network Settings...${NC}"
 {
@@ -191,9 +227,12 @@ echo -e "${NC}"
 } &> /dev/null
 echo -ne '[###################] (100%)\n'
 
+clear
+
 #Generating Random Password for tourd JSON RPC
 rpcuser=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 rpcpassword=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
 
 echo -e "${YELLOW}=====================================================${NC}"
 echo -e "${YELLOW}=====================================================${NC}"
@@ -203,14 +242,14 @@ echo -e
 echo -e "${RED}Some providers do not allow you to install swap!${NC}"
 echo -e 
 echo -e "${GREEN}If you have VPS with locked swap select N to continue${NC}"
+echo -e
 echo -e "${GREEN}If you need to install SWAP or are unsure select Y${NC}"
 echo -e
 echo -e "${YELLOW}=====================================================${NC}"
 echo -e "${YELLOW}=====================================================${NC}"
 echo -e
+echo -e "Please enter your selection :"
 
-
-echo -e "${GREEN}Do you wish to install SWAP Y or N ?${NC} \n"
  read SWAP
  
 	if [[ $SWAP =~ "y" ]] ; then
@@ -235,54 +274,95 @@ echo -e "${GREEN}Do you wish to install SWAP Y or N ?${NC} \n"
 	fi
 	clear
 
- #Extracting Daemon
-cd ~
-mkdir ~/TOUR-MN-setup/touriva_binaries
-cd ~/TOUR-MN-setup/touriva_binaries
-sudo wget https://github.com/Touriva/TOUR/releases/download/v1.1.0/Tour_linux1.1.0-1604.tar.gz
-sudo dtrx -n -f Tour_linux1.1.0-1604.tar.gz
-rm -rf Tour_linux1.1.0-1604.tar.gz
-sudo rm tour-qt
+####TEST FOR OPTION TO INSTALL FOR UBUNTU 18 OR 16 
+
+echo -e "${YELLOW}Detect Ubuntu Version${NC}"
+
+	if [[ $(lsb_release -d) == *18.04* ]]; then
+	UBUNTU_VERSION=18
+		#Extracting Daemon
+		cd ~/$FOLDER
+		sudo wget $SOURCEA
+		sudo dtrx -n -f $ARCHIVEA
+		rm -rf $ARCHIVEA
+		clear
+		echo -e
+		echo -e "${GREEN}Installing for Ubuntu Ver 18.04${NC}"
+		echo -e
+	elif [[ $(lsb_release -d) == *16.04* ]]; then
+		UBUNTU_VERSION=16
+			#Extracting Daemon
+			cd ~/$FOLDER
+			sudo wget $SOURCEB
+			sudo dtrx -n -f $ARCHIVEB
+			rm -rf $ARCHIVEB
+			clear
+			echo -e
+			echo -e "${GREEN}Installing for Ubuntu Ver 16.04${NC}"
+			echo -e
+	else
+	echo -e "${RED}You are not running Ubuntu 18.04 or 16.04 - Installation is cancelled.${NC}"
+	echo -e
+	echo -e "${RED}Please choose a VPS with Ubuntu 16.04 or 18.04 to use this script!${NC}"
+	
+	exit 1
+
+	fi
+	
+#### END TEST FOR VERSION
+
+
 
  
  stop_daemon
  
  # Deploy binaries to /usr/bin
- sudo cp tour* /usr/bin/
- sudo chmod 755 -R ~/TOUR-MN-setup
- sudo chmod 755 /usr/bin/tour*
+ cd ~/$FOLDER/$FOLDER2/
+ sudo cp $NAME2* /usr/bin/
+ sudo chmod 755 -R ~/$FOLDER
+ sudo chmod 755 /usr/bin/$NAME2*
  
  # Deploy masternode monitoring script
- cp ~/TOUR-MN-setup/tourmon.sh /usr/local/bin/
- sudo chmod 711 /usr/local/bin/tourmon.sh
+ cp ~/$FOLDER/$MONITOR /usr/local/bin/
+ sudo chmod 711 /usr/local/bin/$MONITOR
  
  #Create datadir
- if [ ! -f ~/.$HIDDEN/$CONF ]; then 
- 	sudo mkdir ~/.$HIDDEN
+ if [ ! -f ~/$HIDDEN/$CONF ]; then 
+ 	sudo mkdir ~/$HIDDEN
  fi
 
 echo -e "${YELLOW}Creating $CONF...${NC}"
 
 # If genkey was not supplied in command line, we will generate private key on the fly
 if [ -z $genkey ]; then
-    cat <<EOF > ~/.$HIDDEN/$CONF
+    cat <<EOF > ~/$HIDDEN/$CONF
 rpcuser=$rpcuser
 rpcpassword=$rpcpassword
 EOF
 
-    sudo chmod 755 -R ~/.$HIDDEN/$CONF
+    sudo chmod 755 -R ~/$HIDDEN/$CONF
 
     #Starting daemon first time just to generate masternode private key
     $DAEMON -daemon
-echo -ne '[##                 ] (15%)\r'
-sleep 6
-echo -ne '[######             ] (30%)\r'
-sleep 9
-echo -ne '[########           ] (45%)\r'
-sleep 6
-echo -ne '[##############     ] (72%)\r'
+echo -ne '[#         ] (10%)\r'
 sleep 10
-echo -ne '[###################] (100%)\r'
+echo -ne '[##        ] (20%)\r'
+sleep 10
+echo -ne '[###       ] (30%)\r'
+sleep 10 
+echo -ne '[####      ] (40%)\r'
+sleep 10
+echo -ne '[#####     ] (50%)\r'
+sleep 10
+echo -ne '[######    ] (60%)\r'
+sleep 10
+echo -ne '[#######   ] (70%)\r'
+sleep 10
+echo -ne '[########  ] (80%)\r'
+sleep 10
+echo -ne '[######### ] (90%)\r'
+sleep 10
+echo -ne '[##########] (100%)\r'
 echo -ne '\n'
 
     #Generate masternode private key
@@ -299,7 +379,7 @@ echo -ne '\n'
 fi
 
 # Create $CONF
-cat <<EOF > ~/.$HIDDEN/$CONF
+cat <<EOF > ~/$HIDDEN/$CONF
 rpcuser=$rpcuser
 rpcpassword=$rpcpassword
 rpcport=$RPC
@@ -312,11 +392,14 @@ maxconnections=10
 externalip=$publicip:$PORT
 masternode=1
 masternodeprivkey=$genkey
+addnode=$ADDNODEA
+addnode=$ADDNODEB
+addnode=$ADDNODEC
 EOF
 
 #Finally, starting daemon with new $CONF
 $DAEMON -daemon
-delay 5
+delay 10
 
 #Setting auto start cron job daemon
 cronjob="@reboot sleep 30 && $DAEMON -daemon"
@@ -360,7 +443,6 @@ Your initial Masternode Status may read:
     ${YELLOW}Node just started, not yet activated${NC} or
     ${YELLOW}Node  is not in masternode list${NC}, which is normal and expected.
 2) Wait at least until 'IsBlockchainSynced' status becomes 'true' or status 999.
-
 At this point you can go to your wallet and issue a start
 command by either using Debug Console:
     Tools->Debug Console-> enter: ${YELLOW}masternode start-alias mn1${NC}
@@ -373,7 +455,6 @@ Once completed step (2), return to this VPS console and wait for the
 Masternode Status to change to: 'Masternode successfully started'.
 This will indicate that your masternode is fully functional and
 you can celebrate this achievement!
-
 Currently your masternode is syncing with the $TICKER network...
 The following screen will display in real-time
 the list of peer connections, the status of your masternode,
@@ -387,20 +468,20 @@ ${GREEN}...scroll up to see previous screens...${NC}
 Here are some useful commands and tools for masternode troubleshooting:
 ========================================================================
 To view masternode configuration produced by this script in $CONF:
-${YELLOW}cat ~/.$HIDDEN/$CONF${NC}
+${YELLOW}cat ~/$HIDDEN/$CONF${NC}
 Here is your $CONF generated by this script:
 -------------------------------------------------${YELLOW}"
-cat ~/.$HIDDEN/$CONF
+cat ~/$HIDDEN/$CONF
 echo -e "${NC}-------------------------------------------------
 NOTE: To edit $CONF, first stop the $DAEMON daemon,
 then edit the $CONF file and save it in nano: (Ctrl-X + Y + Enter),
 then start the $DAEMON daemon back up:
              to stop:   ${YELLOW}$CLI stop${NC}
-             to edit:   ${YELLOW}nano ~/.$HIDDEN/$CONF${NC}
-             to start:  ${YELLOW}$DAEMON$ -daemon{NC}
+             to edit:   ${YELLOW}nano ~/$HIDDEN/$CONF${NC}
+             to start:  ${YELLOW}$DAEMON -daemon{NC}
 ========================================================================
 To view LSR debug log showing all MN network activity in realtime:
-             ${YELLOW}tail -f ~/.$HIDDEN/debug.log${NC}
+             ${YELLOW}tail -f ~/$HIDDEN/debug.log${NC}
 ========================================================================
 To monitor system resource utilization and running processes:
                    ${YELLOW}htop${NC}
@@ -411,14 +492,12 @@ sync status etc. in real-time, run the $MONITOR script:
 or just type 'node' and hit <TAB> to autocomplete script name.
 ========================================================================
 Enjoy your $TICKER Masternode and thanks for using this setup script!
-
 If you found this script useful, please donate to : 
 ${GREEN}no donations at this time ${NC}
 ...and make sure to check back for updates!
-
 Contact Twystidceed#4126 on discord if you need additional support
 "
-delay 30
+delay 10
 # Run $MONITOR
 sudo $MONITOR
 
